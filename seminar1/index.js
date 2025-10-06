e21f4cfef244d7fbf3479a77b0ff130a499f551c
Bogdan Georegscu <bogdangeo2015@gmail.com>
2025-10-06 12:34:07 +0300

    Seminar1: Server

---
+++ seminar1/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+  res.send('Hello Web!')
+})
+
+app.get('/ping', (req, res) => {
+  res.send('pong')
+})
+
+app.listen(8080, () => {
+  console.log('The SERVER is ready at http://localhost:8080')
+})
