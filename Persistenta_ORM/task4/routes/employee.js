76218dfdabe27c5e317de75300aad171a3c508ad
Bogdan Georegscu <bogdangeo2015@gmail.com>
2025-12-07 01:00:43 +0200

    Upload Persistenta_ORM plus altele

---
+++ Persistenta_ORM/task4/routes/employee.js
@@ -0,0 +1,70 @@
+const { Op } = require("sequelize");
+const Employee = require("../models/employee");
+
+const router = require("express").Router();
+
+router
+  .route("/employees")
+  .get(async (req, res) => {
+    // get all
+    try {
+      const employees = await Employee.findAll({});
+      return res.status(200).json(employees);
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  })
+  .post(async (req, res) => {
+    // create
+    // console.log("req.body :>> ", req.body);
+    try {
+      const newEmployee = await Employee.create(req.body);
+      return res.status(200).json(newEmployee);
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  });
+ 
+router
+  .route("/employees/:id")
+  .get(async (req, res) => {
+    // get by id
+    // filtrarea dupa un camp
+    const employee = await Employee.findOne({
+      where: { id: req.params.id },
+    });
+    if (employee) {
+      return res.status(200).json(employee);
+    } else {
+      return res
+        .status(404)
+        .json({ error: `Employee with id ${req.params.id} does not exists` });
+    }
+  })
+  .put(async (req, res) => {
+    const employee = await Employee.findByPk(req.params.id);
+      if (employee) {
+        return res.status(200).json(await employee.update(req.body));
+    } else {
+      return res
+        .status(404)
+        .json({ error: `Employee with id ${req.params.id} does not exists` });
+    }
+  })
+  .delete(async (req, res) => {
+    try {
+      const employee = await Employee.findByPk(req.params.id);
+      if (employee) {
+        await employee.destroy();
+        return res.status(200).json({message: `Employee {$req.params.id} succesfully deleted`});
+      } else {
+      return res
+        .status(404)
+        .json({ error: `Employee with id ${req.params.id} does not exists` });
+      }
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  });
+
+module.exports = router;
